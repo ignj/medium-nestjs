62c17222bfebb13f8ab698811469e90a56af7b1e
ignj <ignacio.jocano@gmail.com>
2021-09-12 18:13:31 -0300

    add orm config

---
+++ src/ormconfig.ts
@@ -0,0 +1,12 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'admin',
+  database: 'mediumclone',
+};
+
+export default config;
