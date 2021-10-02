f7a9096b2242143ac2f641fc0129f2dd3d15aaca
ignj <ignacio.jocano@gmail.com>
2021-10-02 18:52:32 -0300

    add article entitie and relationship

---
+++ src/article/article.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Post } from '@nestjs/common';
+import { ArticleService } from './article.service';
+
+@Controller('articles')
+export class ArticleController {
+  constructor(private readonly articleService: ArticleService) {}
+
+  @Post()
+  async create() {}
+}
